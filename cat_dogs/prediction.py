89cd1bba98a1a61b9e4ccd46a40a88b82b3d239a
Lornatang <shiyipaisizuo@gmail.com>
2018-08-13 09:27:43 +0800

    optimizer parameter update torch 0.4.1

---
+++ cat_dogs/prediction.py
@@ -0,0 +1,12 @@
+"""
+# author: shiyipaisizuo
+# contact: shiyipaisizuo@gmail.com
+# file: prediction.py
+# time: 2018/8/13 09:24
+# license: MIT
+"""
+
+from . import train
+
+if __name__ == '__main__':
+    train.val()
