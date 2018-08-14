98e07c7f50f515126d78177e2808eb6164c6728b
Lornatang <shiyipaisizuo@gmail.com>
2018-08-14 09:06:32 +0800

    optimizer parameter update torch 0.4.1

--- cat_dogs/train.py
+++ cat_dogs/train.py
@@ -67,6 +67,54 @@ test_loader = torch.utils.data.DataLoader(dataset=test_datasets,
                                           shuffle=True)
 
 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+
+            nn.Conv2d(256, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.75),
+            nn.Linear(in_features=4608, out_features=512, bias=True),
+            nn.ReLU(True),
+            nn.Dropout(p=0.75),
+            nn.Linear(in_features=512, out_features=256, bias=True),
+            nn.ReLU(True),
+            nn.Linear(in_features=256, out_features=2, bias=True),
+        )
+
+    def forward(self, x):
+        out = self.features(x)
+
+        dense = out.view(out.size(0), -1)
+
+        out = self.classifier(dense)
+
+        return out
+
+
 def train():
     print(f"Train numbers:{len(train_datasets)}")
     print(f"Val numbers:{len(test_datasets)}")
@@ -154,4 +202,4 @@ def val():
 
 
 if __name__ == '__main__':
-    train()
+    val()
