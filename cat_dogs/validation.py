2136908833e50069d2b1012e43cc48ea373884e3
Lornatang <shiyipaisizuo@gmail.com>
2018-08-16 22:51:55 +0800

    optimizer parameter update torch 0.4.1

--- cat_dogs/validation.py
+++ cat_dogs/validation.py
@@ -11,7 +11,6 @@ import os
 
 import torch
 import torchvision
-from torch import nn
 from torchvision import transforms
 
 # Device configuration
@@ -20,7 +19,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 parser = argparse.ArgumentParser("""Image classifical!""")
 parser.add_argument('--path', type=str, default='../data/catdog/',
                     help="""image dir path default: '../data/catdog/'.""")
-parser.add_argument('--batch_size', type=int, default=64,
+parser.add_argument('--batch_size', type=int, default=1,
                     help="""Batch_size default:64.""")
 parser.add_argument('--num_classes', type=int, default=2,
                     help="""num classes""")
@@ -55,121 +54,6 @@ val_loader = torch.utils.data.DataLoader(dataset=val_datasets,
 item = train_datasets.class_to_idx
 
 
-class Net(nn.Module):
-    def __init__(self, category=args.num_classes):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 7, 2, 3)  # Size / 2
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu1 = nn.ReLU(True)
-        self.maxpool = nn.MaxPool2d(3, 2, 1)  # Size / 2
-
-        self.layer1 = nn.Sequential(
-            # BasicBlock 1
-            nn.Conv2d(64, 64, 3, 1, 1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(True),
-
-            nn.Conv2d(64, 64, 3, 1, 1),
-            nn.BatchNorm2d(64),
-
-            # BasicBlock 2
-            nn.Conv2d(64, 64, 3, 1, 1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(True),
-
-            nn.Conv2d(64, 64, 3, 1, 1),
-            nn.BatchNorm2d(64),
-        )  # Size / 2
-
-        self.layer2 = nn.Sequential(
-            # BasicBlock 1
-            nn.Conv2d(64, 128, 3, 2, 1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-
-            nn.Conv2d(64, 128, 3, 1, 1),
-            nn.BatchNorm2d(128),
-
-            # downsample
-            nn.Conv2d(64, 128, 1, 2, 0),
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-
-            # BasicBlock 2
-            nn.Conv2d(128, 128, 3, 1, 1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-
-            nn.Conv2d(128, 128, 3, 1, 1),
-            nn.BatchNorm2d(128)
-        )  # Size / 2
-
-        self.layer3 = nn.Sequential(
-            # BasicBlock 1
-            nn.Conv2d(128, 256, 3, 2, 1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-
-            nn.Conv2d(256, 256, 3, 1, 1),
-            nn.BatchNorm2d(256),
-
-            # downsample
-            nn.Conv2d(128, 256, 1, 2, 0),
-            nn.BatchNorm2d(256),
-
-            # BasicBlock 2
-            nn.Conv2d(256, 256, 3, 1, 1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-
-            nn.Conv2d(256, 256, 3, 1, 1),
-            nn.BatchNorm2d(256),
-        )  # Size / 2
-
-        self.layer4 = nn.Sequential(
-            # BasicBlock 1
-            nn.Conv2d(256, 512, 3, 1, 1),
-            nn.atchNorm2d(512),
-            nn.ReLU(True),
-
-            nn.Conv2d(512, 512, 3, 1, 1),
-            nn.BatchNorm2d(512),
-
-            # downsample
-            nn.Conv2d(256, 512, 1, 2),
-            nn.BatchNorm2d(512),
-
-            # BasicBlock 2
-            nn.Conv2d(512, 512, 3, 1, 1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-
-            nn.Conv2d(512, 512, 3, 1, 1),
-            nn.BatchNorm2d(512),
-        )  # Size / 2
-
-        self.avgpool = nn.AvgPool2d(4, 1)  # Size = (Size-7+1)
-
-        self.fc = nn.Linear(512, category)
-
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu1(out)
-        out = self.maxpool(out)
-
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-
-        out = self.avgpool(out)
-        dense = out.reshape(out.size(0), -1)
-        out = self.fc(dense)
-
-        return out
-
-
 def val():
     # Load model
     if torch.cuda.is_available():
@@ -185,7 +69,9 @@ def val():
         # equal prediction and acc
         _, predicted = torch.max(outputs.data, 1)
         di = {v: k for k, v in item.items()}
+
         pred = di[int(predicted[0])]
+
         file = str(val_datasets.imgs[i])[2:-5]
 
         print(f"{i+1}.({file}) is {pred}!")
