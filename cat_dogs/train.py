33c69d94a41bd3bb57e114b5a46c2767cf39c251
Lornatang <shiyipaisizuo@gmail.com>
2018-08-16 13:05:20 +0800

    sort import

--- cat_dogs/train.py
+++ cat_dogs/train.py
@@ -8,14 +8,12 @@
 
 import argparse
 import os
-
 import time
+
 import torch
 import torchvision
-from torch import nn
-from torch import optim
-from torchvision import models
-from torchvision import transforms
+from torch import nn, optim
+from torchvision import models, transforms
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -61,121 +59,6 @@ train_datasets = torchvision.datasets.ImageFolder(root=args.path + 'train/',
 train_loader = torch.utils.data.DataLoader(dataset=train_datasets,
                                            batch_size=args.batch_size,
                                            shuffle=True)
-
-
-# class Net(nn.Module):
-#     def __init__(self, category=args.num_classes):
-#         super(Net, self).__init__()
-#         self.conv1 = nn.Conv2d(3, 64, 7, 2, 3)  # Size / 2
-#         self.bn1 = nn.BatchNorm2d(64)
-#         self.relu1 = nn.ReLU(True)
-#         self.maxpool = nn.MaxPool2d(3, 2, 1)  # Size / 2
-#
-#         self.layer1 = nn.Sequential(
-#             # BasicBlock 1
-#             nn.Conv2d(64, 64, 3, 1, 1),
-#             nn.BatchNorm2d(64),
-#             nn.ReLU(True),
-#
-#             nn.Conv2d(64, 64, 3, 1, 1),
-#             nn.BatchNorm2d(64),
-#
-#             # BasicBlock 2
-#             nn.Conv2d(64, 64, 3, 1, 1),
-#             nn.BatchNorm2d(64),
-#             nn.ReLU(True),
-#
-#             nn.Conv2d(64, 64, 3, 1, 1),
-#             nn.BatchNorm2d(64),
-#         )  # Size / 2
-#
-#         self.layer2 = nn.Sequential(
-#             # BasicBlock 1
-#             nn.Conv2d(64, 128, 3, 2, 1),
-#             nn.BatchNorm2d(128),
-#             nn.ReLU(True),
-#
-#             nn.Conv2d(128, 128, 3, 1, 1),
-#             nn.BatchNorm2d(128),
-#
-#             # downsample
-#             nn.Conv2d(64, 128, 1, 2, 0),
-#             nn.BatchNorm2d(128),
-#             nn.ReLU(True),
-#
-#             # BasicBlock 2
-#             nn.Conv2d(128, 128, 3, 1, 1),
-#             nn.BatchNorm2d(128),
-#             nn.ReLU(True),
-#
-#             nn.Conv2d(128, 128, 3, 1, 1),
-#             nn.BatchNorm2d(128)
-#         )  # Size / 2
-#
-#         self.layer3 = nn.Sequential(
-#             # BasicBlock 1
-#             nn.Conv2d(128, 256, 3, 2, 1),
-#             nn.BatchNorm2d(256),
-#             nn.ReLU(True),
-#
-#             nn.Conv2d(256, 256, 3, 1, 1),
-#             nn.BatchNorm2d(256),
-#
-#             # downsample
-#             nn.Conv2d(128, 256, 1, 2, 0),
-#             nn.BatchNorm2d(256),
-#
-#             # BasicBlock 2
-#             nn.Conv2d(256, 256, 3, 1, 1),
-#             nn.BatchNorm2d(256),
-#             nn.ReLU(True),
-#
-#             nn.Conv2d(256, 256, 3, 1, 1),
-#             nn.BatchNorm2d(256),
-#         )  # Size / 2
-#
-#         self.layer4 = nn.Sequential(
-#             # BasicBlock 1
-#             nn.Conv2d(256, 512, 3, 1, 1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU(True),
-#
-#             nn.Conv2d(512, 512, 3, 1, 1),
-#             nn.BatchNorm2d(512),
-#
-#             # downsample
-#             nn.Conv2d(256, 512, 1, 2),
-#             nn.BatchNorm2d(512),
-#
-#             # BasicBlock 2
-#             nn.Conv2d(512, 512, 3, 1, 1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU(True),
-#
-#             nn.Conv2d(512, 512, 3, 1, 1),
-#             nn.BatchNorm2d(512),
-#         )  # Size / 2
-#
-#         self.avgpool = nn.AvgPool2d(4, 1)  # Size = (Size-7+1)
-#
-#         self.fc = nn.Linear(512, category)
-#
-#     def forward(self, x):
-#         out = self.conv1(x)
-#         out = self.bn1(out)
-#         out = self.relu1(out)
-#         out = self.maxpool(out)
-#
-#         out = self.layer1(out)
-#         out = self.layer2(out)
-#         out = self.layer3(out)
-#         out = self.layer4(out)
-#
-#         out = self.avgpool(out, 4)
-#         dense = out.reshape(out.size(0), -1)
-#         out = self.fc(dense)
-#
-#         return out
 
 
 def train():
