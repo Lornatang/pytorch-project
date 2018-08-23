e1b453f557d249a6562cb31fbeb81f2c30bace93
GPUworkstation <181913143@qq.com>
2018-08-23 10:55:31 +0800

    optimizer update  torch 0.4.1

--- cat_dogs/train.py
+++ cat_dogs/train.py
@@ -21,8 +21,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 parser = argparse.ArgumentParser("""Image classifical!""")
 parser.add_argument('--path', type=str, default='../data/catdog/',
                     help="""image dir path default: '../data/catdog/'.""")
-parser.add_argument('--epochs', type=int, default=10,
-                    help="""Epoch default:50.""")
+parser.add_argument('--epochs', type=int, default=20,
+                    help="""Epoch default:20.""")
 parser.add_argument('--batch_size', type=int, default=64,
                     help="""Batch_size default:64.""")
 parser.add_argument('--lr', type=float, default=0.0001,
@@ -33,7 +33,7 @@ parser.add_argument('--model_path', type=str, default='../../models/pytorch/cat_
                     help="""Save model path""")
 parser.add_argument('--model_name', type=str, default='catdog.pth',
                     help="""Model name.""")
-parser.add_argument('--display_epoch', type=int, default=1)
+parser.add_argument('--display_epoch', type=int, default=2)
 
 args = parser.parse_args()
 
@@ -45,19 +45,25 @@ transform = transforms.Compose([
     transforms.Resize(128),  # 将图像转化为800 * 800
     transforms.RandomHorizontalFlip(p=0.75),  # 有0.75的几率随机旋转
     transforms.RandomCrop(114),  # 从图像中裁剪一个24 * 24的
-    transforms.ColorJitter(brightness=1, contrast=2, saturation=3, hue=0),  # 给图像增加一些随机的光照
     transforms.ToTensor(),  # 将numpy数据类型转化为Tensor
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # 归一化
 ])
 
 
 # Load data
-train_datasets = torchvision.datasets.ImageFolder(root=args.path + 'val/',
+train_datasets = torchvision.datasets.ImageFolder(root=args.path + 'train/',
                                                   transform=transform)
 
 train_loader = torch.utils.data.DataLoader(dataset=train_datasets,
                                            batch_size=args.batch_size,
                                            shuffle=True)
+
+test_datasets = torchvision.datasets.ImageFolder(root=args.path + 'test/',
+                                                 transform=transform)
+
+test_loader = torch.utils.data.DataLoader(dataset=train_datasets,
+                                          batch_size=args.batch_size,
+                                          shuffle=True)
 
 
 def train():
@@ -98,28 +104,28 @@ def train():
                   f"Loss: {loss.item():.8f}, "
                   f"Time: {(end-start) * args.display_epoch:.1f}sec!")
 
-            # model.eval()
-            #
-            # correct_prediction = 0.
-            # total = 0
-            # for images, labels in train_loader:
-            #     # to GPU
-            #     images = images.to(device)
-            #     labels = labels.to(device)
-            #     # print prediction
-            #     outputs = model(images)
-            #     # equal prediction and acc
-            #     _, predicted = torch.max(outputs.data, 1)
-            #     # val_loader total
-            #     total += labels.size(0)
-            #     # add correct
-            #     correct_prediction += (predicted == labels).sum().item()
-            #
-            # print(f"Acc: {(correct_prediction / total):4f}")
+            model.eval()
 
-    # Save the model checkpoint
-    torch.save(model, args.model_path + args.model_name)
-    print(f"Model save to {args.model_path + args.model_name}.")
+            correct_prediction = 0.
+            total = 0
+            for images, labels in test_loader:
+                # to GPU
+                images = images.to(device)
+                labels = labels.to(device)
+                # print prediction
+                outputs = model(images)
+                # equal prediction and acc
+                _, predicted = torch.max(outputs.data, 1)
+                # val_loader total
+                total += labels.size(0)
+                # add correct
+                correct_prediction += (predicted == labels).sum().item()
+
+            print(f"Acc: {(correct_prediction / total):4f}")
+
+        # Save the model checkpoint
+        torch.save(model, args.model_path + args.model_name)
+        print(f"Model save to {args.model_path + args.model_name}.")
 
 
 if __name__ == '__main__':
