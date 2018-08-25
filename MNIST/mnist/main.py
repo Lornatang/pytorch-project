cb4ff4e3d54ba231ddfc515f0d14286139d03ae8
Lornatang <shiyipaisizuo@gmail.com>
2018-08-25 09:00:03 +0800

    update main.py

--- MNIST/mnist/main.py
+++ MNIST/mnist/main.py
@@ -30,7 +30,7 @@ parser.add_argument('--batch_size', type=int, default=128,
                     help="""batch size. Default=128""")
 parser.add_argument('--lr', type=float, default=0.0001,
                     help="""learing_rate. Default=0.0001""")
-parser.add_argument('--model_path', type=str, default='../../models/pytorch/MNIST/',
+parser.add_argument('--model_path', type=str, default='../../../models/pytorch/MNIST/',
                     help="""Save model path""")
 parser.add_argument('--model_name', type=str, default='mnist.pth',
                     help="""Model name""")
