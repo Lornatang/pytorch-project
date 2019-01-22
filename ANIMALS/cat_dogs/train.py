ea466b59ea2b1a2c9131af385f7b98042e491ea1
Lornatang <shiyipaisizuo@gmail.com>
2019-01-22 15:51:55 +0800

    update model .use alexnet

--- ANIMALS/cat_dogs/train.py
+++ ANIMALS/cat_dogs/train.py
@@ -6,82 +6,116 @@
 # license: MIT
 """
 
-import argparse
 import os
-import time
 
+import time
 import torch
 import torchvision
 from torch import nn, optim
+from torch.utils import data
 from torchvision import transforms
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-parser = argparse.ArgumentParser("""Image classifical!""")
-parser.add_argument('--path', type=str, default='../../data/ANIMALS/cat_dogs/',
-                    help="""image dir path default: '../../data/ANIMALS/cat_dogs/'.""")
-parser.add_argument('--epochs', type=int, default=10,
-                    help="""Epoch default:10.""")
-parser.add_argument('--batch_size', type=int, default=128,
-                    help="""Batch_size default:128.""")
-parser.add_argument('--lr', type=float, default=1e-4,
-                    help="""learning_rate. Default=1e-4""")
-parser.add_argument('--num_classes', type=int, default=2,
-                    help="""num classes. Default: 2.""")
-parser.add_argument('--model_path', type=str, default='../../../models/pytorch/ANIMALS/cat_dogs/',
-                    help="""Save model path""")
-parser.add_argument('--model_name', type=str, default='catdog.pth',
-                    help="""Model name.""")
-parser.add_argument('--display_epoch', type=int, default=1)
+WORK_DIR = '../../data/ANIMALS/cat_dogs/'
+NUM_EPOCHS = 10
+BATCH_SIZE = 64
+LEARNING_RATE = 1e-4
+NUM_CLASSES = 2
 
-args = parser.parse_args()
+MODEL_PATH = '../../../models/pytorch/ANIMALS/cat_dogs/'
+MODEL_NAME = 'catdog.pth'
+
 
 # Create model
-if not os.path.exists(args.model_path):
-    os.makedirs(args.model_path)
+if not os.path.exists(MODEL_PATH):
+    os.makedirs(MODEL_PATH)
 
 transform = transforms.Compose([
-    transforms.Resize(128),  # 将图像转化为800 * 800
-    transforms.RandomHorizontalFlip(p=0.75),  # 有0.75的几率随机旋转
-    transforms.RandomCrop(114),  # 从图像中裁剪一个24 * 24的
+    transforms.Resize(224),  # 将图像转化为224 * 224
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),  # 将numpy数据类型转化为Tensor
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # 归一化
 ])
 
 
 # Load data
-train_datasets = torchvision.datasets.ImageFolder(root=args.path + 'train/',
+train_datasets = torchvision.datasets.ImageFolder(root=WORK_DIR + 'train/',
                                                   transform=transform)
 
 train_loader = torch.utils.data.DataLoader(dataset=train_datasets,
-                                           batch_size=args.batch_size,
+                                           batch_size=BATCH_SIZE,
                                            shuffle=True)
 
-test_datasets = torchvision.datasets.ImageFolder(root=args.path + 'val/',
+test_datasets = torchvision.datasets.ImageFolder(root=WORK_DIR + 'val/',
                                                  transform=transform)
 
-test_loader = torch.utils.data.DataLoader(dataset=train_datasets,
-                                          batch_size=args.batch_size,
+test_loader = torch.utils.data.DataLoader(dataset=test_datasets,
+                                          batch_size=BATCH_SIZE,
                                           shuffle=True)
+
+
+class AlexNet(nn.Module):
+
+    def __init__(self, num_classes=NUM_CLASSES):
+        super(AlexNet, self).__init__()
+        self.layer1 = torch.nn.Sequential(
+            torch.nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(3, 2)
+        )
+        self.layer2 = torch.nn.Sequential(
+            torch.nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(3, 2)
+        )
+        self.layer3 = torch.nn.Sequential(
+            torch.nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            torch.nn.ReLU(),
+        )
+        self.layer4 = torch.nn.Sequential(
+            torch.nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            torch.nn.ReLU(),
+        )
+        self.layer5 = torch.nn.Sequential(
+            torch.nn.Conv2d(256, 256, 3, 1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(3, 2)
+        )
+        self.dense = torch.nn.Sequential(
+            torch.nn.Dropout(0.5),
+            torch.nn.Linear(9216, 4096),
+            torch.nn.ReLU(),
+
+            torch.nn.Dropout(0.5),
+            torch.nn.Linear(4096, 4096),
+            torch.nn.ReLU(),
+
+            torch.nn.Linear(4096, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = x.view(x.size(0), -1)
+        out = self.dense(x)
+        return out
 
 
 def main():
     print(f"Train numbers:{len(train_datasets)}")
-
-    # Load model
-    if torch.cuda.is_available():
-        model = torch.load(args.model_path + args.model_name).to(device)
-    else:
-        model = torch.load(args.model_path + args.model_name, map_location='cpu')
-    print(model)
-    # cast
-    cast = nn.CrossEntropyLoss().to(device)
+    model = AlexNet()
+    # cost
+    cost = nn.CrossEntropyLoss().to(device)
     # Optimization
-    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-8)
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=1e-8)
 
-    for epoch in range(1, args.epochs + 1):
-        model.train()
+    for epoch in range(1, NUM_EPOCHS + 1):
+        # model.train()
         # start time
         start = time.time()
         for images, labels in train_loader:
@@ -90,18 +124,18 @@ def main():
 
             # Forward pass
             outputs = model(images)
-            loss = cast(outputs, labels)
+            loss = cost(outputs, labels)
 
             # Backward and optimize
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        if epoch % args.display_epoch == 0:
+        if epoch % 2 == 0:
             end = time.time()
-            print(f"Epoch [{epoch}/{args.epochs}], "
+            print(f"Epoch [{epoch}/{NUM_EPOCHS}], "
                   f"Loss: {loss.item():.8f}, "
-                  f"Time: {(end-start) * args.display_epoch:.1f}sec!")
+                  f"Time: {(end-start) * 2:.1f}sec!")
 
             model.eval()
 
@@ -114,7 +148,8 @@ def main():
                 # print prediction
                 outputs = model(images)
                 # equal prediction and acc
-                _, predicted = torch.max(outputs.data, 1)
+
+                _, predicted = torch.argmax(outputs.data, 1)
                 # val_loader total
                 total += labels.size(0)
                 # add correct
@@ -123,8 +158,8 @@ def main():
             print(f"Acc: {(correct_prediction / total):4f}")
 
         # Save the model checkpoint
-        torch.save(model, args.model_path + args.model_name)
-    print(f"Model save to {args.model_path + args.model_name}.")
+        torch.save(model, MODEL_PATH + MODEL_NAME)
+    print(f"Model save to {MODEL_PATH + MODEL_NAME}.")
 
 
 if __name__ == '__main__':
