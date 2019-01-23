1759fe9fd609042a9cae1aa43d4e1f3e66f8bcc0
Lornatang <shiyipaisizuo@gmail.com>
2019-01-23 16:07:36 +0800

    update old coe

--- CALTECH/102/train.py
+++ CALTECH/102/train.py
@@ -6,81 +6,74 @@
 # license: MIT
 """
 
-import argparse
 import os
-import time
 
+import time
 import torch
 import torchvision
+from net import Net
 from torch import nn, optim
+from torch.utils import data
 from torchvision import transforms
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-parser = argparse.ArgumentParser("""Image classifical!""")
-parser.add_argument('--path', type=str, default='../../data/CALTECH/102/',
-                    help="""image dir path default: '../../data/CALTECH/102/'.""")
-parser.add_argument('--epochs', type=int, default=10,
-                    help="""Epoch default:10.""")
-parser.add_argument('--batch_size', type=int, default=128,
-                    help="""Batch_size default:128.""")
-parser.add_argument('--lr', type=float, default=1e-4,
-                    help="""learning_rate. Default=1e-4""")
-parser.add_argument('--num_classes', type=int, default=102,
-                    help="""num classes. Default: 102.""")
-parser.add_argument('--model_path', type=str, default='../../../models/pytorch/CALTECH/',
-                    help="""Save model path""")
-parser.add_argument('--model_name', type=str, default='102.pth',
-                    help="""Model name.""")
-parser.add_argument('--display_epoch', type=int, default=1)
+WORK_DIR = '../../data/CALTECH/4/'
+NUM_EPOCHS = 10
+BATCH_SIZE = 64
+LEARNING_RATE = 1e-4
+NUM_CLASSES = 4
 
-args = parser.parse_args()
+MODEL_PATH = '../../../models/pytorch/CALTECH/'
+MODEL_NAME = '4.pth'
 
 # Create model
-if not os.path.exists(args.model_path):
-    os.makedirs(args.model_path)
+if not os.path.exists(MODEL_PATH):
+    os.makedirs(MODEL_PATH)
 
 transform = transforms.Compose([
-    transforms.Resize(128),  # 将图像转化为800 * 800
-    transforms.RandomHorizontalFlip(0.5),  # 有0.75的几率随机旋转
-    transforms.RandomCrop(114),  # 从图像中裁剪一个24 * 24的
+    transforms.Resize(224),  # 将图像转化为224 * 224
+    transforms.RandomHorizontalFlip(),  # 几率随机旋转
     transforms.ToTensor(),  # 将numpy数据类型转化为Tensor
     transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])  # 归一化
 ])
 
 
 # Load data
-train_datasets = torchvision.datasets.ImageFolder(root=args.path + 'train/',
+train_datasets = torchvision.datasets.ImageFolder(root=WORK_DIR + 'train/',
                                                   transform=transform)
 
 train_loader = torch.utils.data.DataLoader(dataset=train_datasets,
-                                           batch_size=args.batch_size,
+                                           batch_size=BATCH_SIZE,
                                            shuffle=True)
+
+val_datasets = torchvision.datasets.ImageFolder(root=WORK_DIR + 'val/',
+                                                transform=transform)
+
+val_loader = torch.utils.data.DataLoader(dataset=train_datasets,
+                                         batch_size=BATCH_SIZE,
+                                         shuffle=True)
 
 
 def main():
     print(f"Train numbers:{len(train_datasets)}")
-    print(f"Test numbers:{len(train_datasets)}")
+    print(f"Val numbers:{len(val_datasets)}")
 
-    # Load model
-    if torch.cuda.is_available():
-        model = torch.load(args.model_path + args.model_name).to(device)
-    else:
-        model = torch.load(args.model_path + args.model_name, map_location='cpu')
-    # model = models.resnet18(pretrained=True).to(device)
-    # model.avgpool = nn.AvgPool2d(4, 1).to(device)
-    # model.fc = nn.Linear(512, args.num_classes).to(device)
-    print(model)
+    model = Net()
     # cast
     cast = nn.CrossEntropyLoss().to(device)
     # Optimization
-    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-8)
+    optimizer = optim.Adam(
+        model.parameters(),
+        lr=LEARNING_RATE,
+        weight_decay=1e-8)
 
-    for epoch in range(1, args.epochs + 1):
-        model.train()
+    for epoch in range(1, NUM_EPOCHS + 1):
+        # model.train()
         # start time
         start = time.time()
+        step = 1
         for images, labels in train_loader:
             images = images.to(device)
             labels = labels.to(device)
@@ -94,34 +87,14 @@ def main():
             loss.backward()
             optimizer.step()
 
-        if epoch % args.display_epoch == 0:
             end = time.time()
-            print(f"Epoch [{epoch}/{args.epochs}], "
+            print(f"Step [{step * 64}/{10 * len(train_datasets) / 64}], "
                   f"Loss: {loss.item():.8f}, "
-                  f"Time: {(end-start) * args.display_epoch:.1f}sec!")
-
-            model.eval()
-
-            correct = 0.
-            total = 0
-            for images, labels in train_loader:
-                # to GPU
-                images = images.to(device)
-                labels = labels.to(device)
-                # print prediction
-                outputs = model(images)
-                # equal prediction and acc
-                _, predicted = torch.max(outputs.data, 1)
-                # val_loader total
-                total += labels.size(0)
-                # add correct
-                correct += (predicted == labels).sum().item()
-
-            print(f"Acc: {correct / total:.4f}.")
+                  f"Time: {(end-start) * 1:.1f}sec!")
 
         # Save the model checkpoint
-        torch.save(model, args.model_path + args.model_name)
-    print(f"Model save to {args.model_path + args.model_name}.")
+        torch.save(model, MODEL_PATH + MODEL_NAME)
+    print(f"Model save to {MODEL_PATH + MODEL_NAME}.")
 
 
 if __name__ == '__main__':
--- CALTECH/154/prediction.py
+++ CALTECH/154/prediction.py
@@ -6,65 +6,59 @@
 # license: MIT
 """
 
-import argparse
 import os
 
 import torch
 import torchvision
+from torch.utils import data
 from torchvision import transforms
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-parser = argparse.ArgumentParser("""Image classifical!""")
-parser.add_argument('--path', type=str, default='../../data/CALTECH/154/',
-                    help="""image dir path default: '../../data/CALTECH/154/'.""")
-parser.add_argument('--batch_size', type=int, default=128,
-                    help="""Batch_size default:128.""")
-parser.add_argument('--num_classes', type=int, default=256,
-                    help="""num classes. Default: 154.""")
-parser.add_argument('--model_path', type=str, default='../../../models/pytorch/CALTECH/',
-                    help="""Save model path""")
-parser.add_argument('--model_name', type=str, default='154.pth',
-                    help="""Model name.""")
+WORK_DIR = '../../data/CALTECH/4/'
+NUM_EPOCHS = 10
+BATCH_SIZE = 64
+LEARNING_RATE = 1e-4
+NUM_CLASSES = 4
 
-args = parser.parse_args()
+MODEL_PATH = '../../../models/pytorch/CALTECH/'
+MODEL_NAME = '4.pth'
 
 # Create model
-if not os.path.exists(args.model_path):
-    os.makedirs(args.model_path)
+if not os.path.exists(MODEL_PATH):
+    os.makedirs(MODEL_PATH)
 
 transform = transforms.Compose([
-    transforms.Resize(128),  # 将图像转化为800 * 800
-    transforms.RandomCrop(114),  # 从图像中裁剪一个24 * 24的
+    transforms.Resize(224),  # 将图像转化为800 * 800
     transforms.ToTensor(),  # 将numpy数据类型转化为Tensor
     transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])  # 归一化
 ])
 
 
 # Load data
-test_datasets = torchvision.datasets.ImageFolder(root=args.path + 'train/',
-                                                 transform=transform)
+val_datasets = torchvision.datasets.ImageFolder(root=WORK_DIR + 'val/',
+                                                transform=transform)
 
-test_loader = torch.utils.data.DataLoader(dataset=test_datasets,
-                                          batch_size=args.batch_size,
-                                          shuffle=True)
+val_loader = torch.utils.data.DataLoader(dataset=val_datasets,
+                                         batch_size=BATCH_SIZE,
+                                         shuffle=True)
 
 
 def main():
-    print(f"Test numbers:{len(test_datasets)}")
+    print(f"Val numbers:{len(val_datasets)}")
 
     # Load model
     if torch.cuda.is_available():
-        model = torch.load(args.model_path + args.model_name).to(device)
+        model = torch.load(MODEL_PATH + MODEL_NAME).to(device)
     else:
-        model = torch.load(args.model_path + args.model_name, map_location='cpu')
+        model = torch.load(MODEL_PATH + MODEL_NAME, map_location='cpu')
 
     model.eval()
 
     correct = 0.
     total = 0
-    for images, labels in test_loader:
+    for images, labels in val_loader:
         # to GPU
         images = images.to(device)
         labels = labels.to(device)
@@ -77,7 +71,7 @@ def main():
         # add correct
         correct += (predicted == labels).sum().item()
 
-    print(f"Acc: {100 * correct / total:.4f}.")
+    print(f"Acc: {correct / total:.4f}.")
 
 
 if __name__ == '__main__':
